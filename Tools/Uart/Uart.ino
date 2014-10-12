d9a2b760ca97450b63a54f6b870a9641650bcc89
rrockru <rrock.ru@gmail.com>
2014-10-12 14:30:36 +0400

    Add UART tool

---
+++ Tools/Uart/Uart.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(0, INPUT); //RX 
+ pinMode(1, INPUT);//TX 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
